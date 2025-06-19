d451abbcf47a4765e2cfba6c235019ac7e15b7a1
gojo18888 <sayuramaximum@gmail.com>
2025-06-19 08:32:32 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ownerNumber: '94743826406',    // Your WhatsApp number
+  prefix: '.',                   // Command prefix
+  language: 'en',
+  debug: true,
+  sessionId: '7QEQTYKb#Jgy3TQjN4eKapt683cVQu22LuXnO4olGjEbZyBXJ4RY'         // Session folder name (can change to support multi-session)
+};
